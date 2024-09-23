c965e4e32e4c152037157aff5e675e998f3ff4f8
GeniltonSilva <geniltonsilva4002@gmail.com>
2024-09-23 19:57:28 -0300

    fix: teste

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -7,20 +7,6 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
-  isActive = false;
-
-  toggleSwitch() {
-    this.isActive = !this.isActive;
-  }
-  
-  carditens = [ 
-    { title: 'Intranet', description: 'Description 1', image: 'assets/intranet-BwslL2mt.png' },
-    { title: 'Sysra', description: 'Description 1', image: 'assets/sysra-lseskkPR.png' },
-    { title: 'Senium', description: 'Description 1', image: 'assets/seniumm.png' },
-    { title: 'Avisos', description: 'Description 1', image: 'assets/1710543310570.jpeg' },
-    { title: 'Chaves', description: 'Description 1', image: 'assets/chavesPNG.PNG' },
-    { title: 'Card 4', description: 'Description 1', image: 'assets/intranet-BwslL2mt.png' },
-  ];
 
   
 }
